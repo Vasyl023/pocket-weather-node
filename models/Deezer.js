396a4e4e085183d4fc484abdf3712b615bffe7e7
Danylo Mylian <danylo.mylian@kindgeek.com>
2016-04-06 23:29:58 +0300

    reduce unneeded methods

--- models/Deezer.js
+++ models/Deezer.js
@@ -1,71 +1,56 @@
 const rp = require('request-promise');
 const logger = require('./Logger');
-	class Deezer {
+class Deezer {
 
-		constructor() {
-		}
-
-		search(q) {
-			return rp(`https://api.deezer.com/search/playlist?q=${q}`);
-		}
-
-		getPlaylist(q) {
-			//let weather = ['clear sky', 'broken clouds'];
-			/*
-			if (/(clear sky)|(few|scattered) clouds/.test(q)) {
-				q = ['clear sky', 'sun shining', 'sunny', 'sunshine'];
-			} else if (/(broken|overcast) clouds/.test(q)) {
-				q = ['clouds'];
-			}
-			*/
-			return this.search(q).then((res) => {
-				let plNames = JSON.parse(res).data.map(pl => pl.title);
-				logger.cacheSet('matchingPlaylists', plNames);
-				return this.getTracks(JSON.parse(res))
-			});
-		}
-
-		getTracks(res) {
-			const limit = res.total >= 25 ? 24 : res.total - 1;
-			const idx = Math.floor(Math.random() * (limit + 1));
-			//console.log('\n-----------\nDEEZER RESPONSE:', res.total);
-			//console.log(`INDEX  --> ${idx}`);
-			const playlistData = res.data[idx];
-			logger.cacheSet('playlistData', playlistData);
-
-			const playlistTracksUrl = playlistData['tracklist'];
-
-			//console.log('DATA', res.data[idx]);
-
-			return rp(playlistTracksUrl).then((res) => {
-				return this.processPlaylistTracks(playlistData, JSON.parse(res))
-			});
-
-		}
-
-		processPlaylistTracks(plData, res) {
-
-				const tracks = res.data.map((el) => {
-				return {
-					title: el.title,
-					duration: el.duration,
-					source: el.preview,
-					artist: el.artist.name,
-					cover: el.album.cover_small
-				}
-			});
-
-			plData = {
-				title: plData.title,
-				link: plData.link,
-				cover: plData.picture,
-				author: plData.user.name
-			};
-
-			plData.tracks = tracks;
-			return plData;
-		}
-
+	constructor() {
 	}
+
+	search(q) {
+		return rp(`https://api.deezer.com/search/playlist?q=${q}`);
+	}
+
+	getPlaylist(q) {
+		// get daytime && season also ?
+		var t_q;
+		if (/(clear sky)|(few|scattered) clouds/.test(q)) {
+			t_q = ['clear sky', 'sun shining', 'sunny', 'sunshine'];
+		} else if (/(broken|overcast) clouds/.test(q)) {
+			t_q = ['clouds'];
+		} else if (/drizzle|rain/.test(q)) {
+			t_q = ['rainy']
+		}
+
+
+		return Promise.all(
+			t_q.map((el) => this.search(el))
+		).then((res) => {
+			let plNames = res
+				.map((el) => {
+					return JSON.parse(el).data.map((elInner) => {
+						return {
+							title: elInner.title,
+							url: elInner.tracklist,
+							cover: elInner.picture,
+							author: elInner.user.name
+						}
+					});
+				})
+				.reduce((a, b) => {
+					return a.concat(b);
+				});
+
+			const limit = plNames.length >= 25 ? 24 : plNames.length - 1;
+			const idx = Math.floor(Math.random() * (limit + 1));
+
+			logger.cacheSet('playlists', plNames[idx]);
+			logger.cacheSet('playlists', plNames.map(el => el.title));
+
+			var pl = plNames[idx];
+			const url = plNames[idx].url;
+
+			return rp(url).then((res) => (Object.assign(pl, JSON.parse(res))));
+		});
+	}
+}
 
 module.exports = Deezer;
